697afbc9b9205eafd2eb2a5335bd97f31a0e920f
karthik Ganjam <53421655+karthikganjam99kg@users.noreply.github.com>
2020-07-07 10:52:51 +0530

    Add files via upload

--- feecvr-master/js/script1.js
+++ feecvr-master/js/script1.js
@@ -1,22 +1,34 @@
-async function load1() {
-    document.getElementById("card1").style.display="none";
-    document.getElementByClass("loader").style.display="block";
-    
-    
-    setTimeout(function(){
-        document.getElementById("card1").style.display="block";
-         document.getElementByClass("loader").style.display="none";
-         }, 2000);
-  }
- function load2() {
-    document.getElementById("card2").style.display="none";
-    document.getElementByClass("loader").style.display="block";
-    setTimeout(function(){
-        document.getElementByClass("loader").style.display="none";
-         document.getElementById("card2").style.display="block";
-         }, 1000);
-  }
+$(document).ready(function(){
+  let image = document.getElementById("card1");
+  let image1 = document.getElementById("card2");
+  
+  let loaders = document.getElementsByClassName("loader");
+  let secondPromise = new Promise((resolve, reject) => {
+    image1.style.display="none";
+     
+    setTimeout(() => {
+        image1.style.display = "block";
+        loaders[1].style.display = "none";
+        
+        resolve("Success");
+    }, 6000);
+});
 
+
+
+  let firstPromise = new Promise((resolve, reject) => {
+      image.style.display="none";
+      setTimeout(() => {
+          image.style.display = "block";
+          loaders[0].style.display = "none";
+          
+          resolve(secondPromise);
+      }, 3000);
+  });
   
 
-window.onload=function(){load1().then(load2());};
+  firstPromise.then().then(() => {
+      console.log("Loaded Images Successfully");
+  }).catch();
+
+});
